b851175029217425d27b4cb2eeda387d9a753afb
hemanthkapa <kapa4902@gmail.com>
2024-11-07 12:05:51 -0500

    Check that report components are not initially nit visible

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
@@ -12,4 +12,16 @@ describe("Test the Seeding Report default values", ()=>{
         cy.get("legend")  
             .should("contain", "Set Dates") 
     })
+
+    it("Check that report components are not initially visible", () => {
+        
+        cy.get('[data-cy=filters-panel]')
+            .should('not.exist')
+        cy.get('[data-cy=report-table]')
+            .should('not.exist')
+        cy.get('[data-cy=direct-summary]')
+            .should('not.exist')
+        cy.get('[data-cy=tray-summary]')
+            .should('not.exist')
+    })
 })
