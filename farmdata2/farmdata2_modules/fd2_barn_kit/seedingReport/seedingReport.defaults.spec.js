0ceb8f3297ca7069f7efdeddbe53eaaad92d03b2
sonnguyen123526 <nguyenso.dickinson.edu>
2024-11-07 11:38:29 -0500

    changing visit

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
@@ -1,7 +1,7 @@
 describe("Test the Seeding Report default values", ()=>{
     beforeEach(()=> {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-barn-kit/seedingReport")
     })
     it("Check the page header", ()=> {
         cy.get("[data-cy=text-center]")
