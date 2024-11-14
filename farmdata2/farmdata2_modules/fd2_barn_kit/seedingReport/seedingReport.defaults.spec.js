6617b7712b984cb69e520ac20cfd2cb37a7643d4
hmhngx <nguyehun@dickinson.edu>
2024-11-14 11:35:21 -0500

    complete Cypress testing for the 3rd and 4th tasks in the #10 issue

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
@@ -1,33 +1,48 @@
-describe("Test the Seeding Report default values", ()=>{
+describe("Test the Seeding Report default values", () => {
 
-    beforeEach(()=> {
-        cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-barn-kit/seedingReport")
-    })
-    it("Check the page header", ()=> {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2");
+        cy.visit("/farm/fd2-barn-kit/seedingReport");
+    });
+
+    it("Check the page header", () => {
         cy.get("[data-cy=text-center]")
-        .should("have.text","Seeding Report")
-    })
-    it("Check the default date - end date", ()=> {
-        const today = new Date().toISOString().split("T")[0]
+            .should("have.text", "Seeding Report");
+    });
+
+    it("Check the default date - end date", () => {
+        const today = new Date().toISOString().split("T")[0];
         cy.get("[data-cy=date-range-selection]")
-        .find('input[data-cy="end-date"]').should("have.value", today)
-    })
+            .find('input[data-cy="end-date"]').should("have.value", today);
+    });
+
+    it("Verify Generate Report button label and state", () => {
+        cy.get("[data-cy=generate-rpt-btn]")
+            .should("be.visible")
+            .and("have.text", "Generate Report")
+            .and("not.be.disabled");
+    });
+
+    it("Verify the default start date", () => {
+        const firstDayOfYear = new Date(new Date().getFullYear(), 0, 1).toISOString().split('T')[0];
+
+        cy.get("[data-cy=date-range-selection] input[name='start-date']")
+            .should("have.value", firstDayOfYear);
+    });
 
     it("Check for Set Dates section", () => {
-        cy.get("legend")  
-            .should("contain", "Set Dates") 
-    })
+        cy.get("legend")
+            .should("contain", "Set Dates");
+    });
 
     it("Check that report components are not initially visible", () => {
-        
         cy.get('[data-cy=filters-panel]')
-            .should('not.exist')
+            .should('not.exist');
         cy.get('[data-cy=report-table]')
-            .should('not.exist')
+            .should('not.exist');
         cy.get('[data-cy=direct-summary]')
-            .should('not.exist')
+            .should('not.exist');
         cy.get('[data-cy=tray-summary]')
-            .should('not.exist')
-    })
-})
+            .should('not.exist');
+    });
+});
