0d93c756793e26b6947d66e4b63c74a4a8cd918c
sonnguyen123526 <nguyenso.dickinson.edu>
2024-11-13 23:46:03 -0500

    Commit test default date

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.defaults.spec.js
@@ -1,4 +1,5 @@
 describe("Test the Seeding Report default values", ()=>{
+
     beforeEach(()=> {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-barn-kit/seedingReport")
@@ -6,5 +7,10 @@ describe("Test the Seeding Report default values", ()=>{
     it("Check the page header", ()=> {
         cy.get("[data-cy=text-center]")
         .should("have.text","Seeding Report")
+    })
+    it("Check the default date - end date", ()=> {
+        const today = new Date().toISOString().split("T")[0]
+        cy.get("[data-cy=date-range-selection]")
+        .find('input[data-cy="end-date"]').should("have.value", today)
     })
 })
